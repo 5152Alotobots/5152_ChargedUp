88f8e3a0d52a154ecbe8d5c98b2eb7fdb664e5b1
Rowan <rkortlandt@gmail.com>
2023-03-12 13:58:54 -0400

    added cmd groups removed unused commands

---
+++ src/main/java/frc/robot/ChargedUp/Arm/Cmd/CmdGrp_MoveToPickUpFlipPos.java
@@ -0,0 +1,24 @@
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.ChargedUp.Arm.Cmd;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.ChargedUp.Arm.SubSys_Arm;
+import frc.robot.ChargedUp.Arm.SubSys_Arm;
+
+
+
+public class CmdGrp_MoveToPickUpFlipPos extends ParallelCommandGroup {
+  private final SubSys_Arm m_Arm;
+  /** Creates a new CmdGrp_MoveToTopPos. */
+  public CmdGrp_MoveToPickUpFlipPos(SubSys_Arm armSubSys) {
+
+    m_Arm = armSubSys;
+    
+    addCommands(
+      new Cmd_ArmExtensionPID(armSubSys, 0),
+      new Cmd_ArmRotationPID(armSubSys, 0)
+    );
+  }
+}
