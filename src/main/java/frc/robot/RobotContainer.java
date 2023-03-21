84c33b921e0d115cb54626899a048101375a9a06
chrisdeboer <ccdeboer75@gmail.com>
2023-03-21 19:04:06 -0400

    Missed file

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.ChargedUp.Arm.Cmds_SubSys_Arm.Cmd_SubSys_Arm_JoysticDefault;
 import frc.robot.ChargedUp.Arm.Cmds_SubSys_Arm.Cmd_SubSys_Arm_PosCmd;
 import frc.robot.ChargedUp.Arm.SubSys_Arm;
@@ -163,6 +164,8 @@ public class RobotContainer {
     m_chooser.addOption("rightredescape", m_rightredescape);
 
     SmartDashboard.putData(m_chooser);
+
+    
   }
 
   /**
